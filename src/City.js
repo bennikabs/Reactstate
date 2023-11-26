a2a0a0dfbd486c7b2eedce4bcf534682fc6d7622
Benny <114034060+bennikabs@users.noreply.github.com>
2023-11-26 04:09:36 +0100

    solved challenge 3

--- src/City.js
+++ src/City.js
@@ -2,20 +2,23 @@ import React, {useState} from "react";
 
 export default function City(props) {
 let [temperature, setTemperature]= useState(props.temperature);
+  let[unit, setUnit]= useState("celsius");
 
 function showFahrenheit(event){
   event.preventDefault();
   setTemperature(Math.round((props.temperature * 9) / 5 + 32));
+  setUnit("imperial");
 }
   
 function showCelsius(event){
   event.preventDefault();
   setTemperature(props.temperature);
+  setUnit("celsius");
 }
   
   return (
     <div className="City">
-      It is {temperature}<a href="/" onClick={showCelsius}>°C </a> | <a href="/" onClick={showFahrenheit}> °F </a>in {props.name}
+      It is {temperature} ({unit})<a href="/" onClick={showCelsius}>°C </a> | <a href="/" onClick={showFahrenheit}> °F </a>in {props.name}
     </div>
   );
 }
